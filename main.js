eaf41bfe96e6b110cc2f0467582548c96799b40e
Kalys01 <kalysosmon96.g@gmail.com>
2021-02-01 02:03:04 +0600

    created function timer

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const App = {
     data () {
         return {
-            timer: 10
+            timer: 10,
+            active: 'go'
         }
+    },
+    mounted: function () {
+        setInterval(() => {
+            if (this.timer < 1){
+                this.active == 'stop'
+                this.timer = 11
+            }
+            this.timer--;
+        }, 1000);
     }
 }
 
