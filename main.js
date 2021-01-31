f7a32d60208274e31df12c8ba0d6c66b50b70b88
Kalys01 <kalysosmon96.g@gmail.com>
2021-02-01 02:19:11 +0600

    added light far

--- main.js
+++ main.js
@@ -7,9 +7,16 @@ const App = {
     },
     mounted: function () {
         setInterval(() => {
-            if (this.timer < 1){
-                this.active == 'stop'
-                this.timer = 11
+            if (this.timer < 5){
+                // this.active = null ? this.timer % 2 : 'prepare'
+                if (this.timer % 2){
+                    this.active = null
+                } else {this.active = 'prepare'}
+
+                if (this.timer < 1){
+                    this.timer = 11
+                    this.active = 'go' ? 'stop' : 'go'
+                } 
             }
             this.timer--;
         }, 1000);
